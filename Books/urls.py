28ec7be479c3db86db4a93ecdd9a1730d1b56a06
Radhika <radhatopare20082002@gmail.com>
2025-01-08 23:30:36 +0530

    commited changes

---
+++ Books/urls.py
@@ -0,0 +1,16 @@
+# books/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('book/new/', views.book_create, name='book_create'),
+    path('book/<int:pk>/edit/', views.book_update, name='book_update'),
+    path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+]
